e6c9ca4888b34c7dbb7141053f707537185dd41d
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-12-16 00:18:07 +0600

    Implemented JWT token for secure all the private data and also implemented Verify admin related api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,7 +15,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1bw0ez.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +38,45 @@ async function run() {
     const menuCollection = client.db("foodFestivalDB").collection("menu");
     const reviewsCollection = client.db("foodFestivalDB").collection("reviews");
     const cartsCollection = client.db("foodFestivalDB").collection("carts");
+    const usersCollection = client.db("foodFestivalDB").collection("users");
+
+
+
+    //JWT RELATED API
+    app.post('/jwt',async(req,res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
+        res.send({token});
+    });
+    //MIDDLEWARE
+    const verifyToken =(req,res,next)=>{
+        // console.log('inside varified token',req.headers.authorization);
+        if(!req.headers.authorization){
+        return res.status(401).send({message:'unauthorized access'});
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded=decoded;
+        next();
+      })
+    }
+
+
+
+    // VERIFY ADMIN
+    const verifyAdmin = async(req,res, next)=>{
+        const email = req.decoded.email;
+        const query = {email:email}
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === 'admin'
+        if(!isAdmin){
+          return res.status(403).send({message : ' forbidden access'});
+        }
+        next();
+      }
 
 
 
@@ -55,7 +95,67 @@ async function run() {
         const cartItem = req.body;
         const result = await cartsCollection.insertOne(cartItem);
         res.send(result);
-    })
+    });
+    app.get('/carts',async(req,res)=>{
+        const email = req.query.email;
+        const query ={email:email};
+        const result = await cartsCollection.find(query).toArray();
+        res.send(result);
+    });
+    app.delete('/carts/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await cartsCollection.deleteOne(query);
+        res.send(result)
+    });
+
+
+
+    //  User related API
+    app.get('/users/admin/:email',verifyToken, async(req,res)=>{
+        const email = req.params.email;
+        if(email !== req.decoded.email){
+          return res.status(403).send({message : "forbidden access"})
+        }
+        const query = {email:email}
+        const user = await usersCollection.findOne(query);
+        let admin = false;
+        if(user){
+          admin= user?.role === 'admin';
+        }
+        res.send({admin});
+      });
+    app.post('/users',async (req,res)=>{
+        const user = req.body;
+        const query ={email: user.email}
+        const existingUser = await usersCollection.findOne(query);
+        if(existingUser){
+          return res.send({message: 'user already exists', insertedId : null})
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    });
+    app.get('/users',verifyToken,verifyAdmin,async(req,res)=>{
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    });
+    app.delete('/users/:id',verifyToken,verifyAdmin, async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+    });
+    app.patch('/users/admin/:id', verifyToken,verifyAdmin,async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updatedDoc = {
+          $set :{
+            role : 'admin'
+          }
+        }
+        const result = await usersCollection.updateOne(filter,updatedDoc);
+        res.send(result);
+    });  
 
 
 
